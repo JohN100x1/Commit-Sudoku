fa24974f371204913df2a5f8ba1d3de269dcf269
JohN100x1 <JohN100x1@hotmail.co.uk>
2022-03-05 12:31:09 +0000

    refactor: added type hints

--- src/sudoku_app.py
+++ src/sudoku_app.py
@@ -1,10 +1,7 @@
 import logging
-import random
 from tkinter import Button, LabelFrame, Tk
 
 from sudoku_board import SudokuBoard
-
-DIGITS = {str(i) for i in range(1, 11)}
 
 logger = logging.getLogger(__name__)
 
@@ -13,9 +10,6 @@ class SudokuApp(Tk):
     def __init__(self):
         super().__init__()
         self.title("Sudoku")
-
-        logger.info("Creating Solution box")
-        self.solution = [["" for j in range(9)] for i in range(9)]
 
         logger.info("Creating Frames")
         self.frames = [
@@ -27,95 +21,34 @@ class SudokuApp(Tk):
         self.actions = LabelFrame(text="Actions")
 
         # Creating Actions Buttons
-        self.buttons = ["New Game", "Solve", "Hint"]
-        self.button_frame = []
-        for j, b in enumerate(self.buttons):
-            self.button_frame.append(
+        self.buttons = []
+        for i, action in enumerate(["New Game", "Solve", "Hint"]):
+            self.buttons.append(
                 Button(
                     self.actions,
-                    text=b,
+                    text=action,
                     padx=10,
                     pady=5,
-                    command=lambda x=b: self.button_action(x),
+                    command=lambda x=action: self.button_action(x),
                 )
             )
-            self.button_frame[j].grid(row=1, column=j, padx=38, pady=10)
+            self.buttons[i].grid(row=1, column=i, padx=38, pady=10)
 
         self.actions.grid(row=1, column=1, padx=50, pady=10)
 
         # Generate Board and GUI
-        self.generate_board()
+        self.board.generate()
 
-    def generate_board(self, seed=None, num_clues=17):
-        if seed is not None:
-            random.seed(seed)
-        # Get random positions to keep
-        entry_sample = random.sample(range(81), num_clues)
-        keep_pos = {(x // 9, x % 9) for x in entry_sample}
-        # Refresh the board
-        for i in range(9):
-            for j in range(9):
-                self.board.sv[i][j].set("")
-                self.board.board[i][j]["state"] = "normal"
-        # Solve the board
-        is_solved = self.solve_board(rand=True)
-        # Partially erasing solved board
-        for i in range(9):
-            for j in range(9):
-                self.solution[i][j] = self.board.sv[i][j].get()
-                if (i, j) not in keep_pos:
-                    self.board.sv[i][j].set("")
-                else:
-                    self.board.board[i][j]["state"] = "disabled"
-                self.board.board[i][j]["bg"] = "white"
-        if is_solved:
-            print("Board Generated.")
-
-    def find_empty(self):
-        # Find empty position
-        for i in range(9):
-            for j in range(9):
-                if self.board.sv[i][j].get() == "":
-                    return i, j
-        return None
-
-    def solve_board(self, rand=False):
-        # Find Empty positions
-        empty_pos = self.find_empty()
-        if empty_pos is None:
-            return True
-        else:
-            i, j = empty_pos
-
-        # Try random digits or not
-        digits = [str(i) for i in range(1, 10)]
-        if rand:
-            random.shuffle(digits)
-
-        # Solve via back-tracking
-        for d in digits:
-            if self.board.possible(d, i, j):
-                self.board.sv[i][j].set(d)
-                if self.solve_board(rand=rand):
-                    return True
-                self.board.sv[i][j].set("")
-        return False
-
-    def button_action(self, action):
+    def button_action(self, action: str):
         if action == "New Game":
-            self.generate_board()
+            self.board.generate()
+
         elif action == "Solve":
-            for i in range(9):
-                for j in range(9):
-                    if self.board.sv[i][j].get() == "":
-                        self.board.sv[i][j].set(self.solution[i][j])
+            self.board.solve()
+
         elif action == "Hint":
-            entry_sample = random.sample(range(81), 81)
-            pos = {(x // 9, x % 9) for x in entry_sample}
-            for i, j in pos:
-                if self.board.sv[i][j].get() == "":
-                    self.board.sv[i][j].set(self.solution[i][j])
-                    break
+            self.board.hint()
+
         else:
             pass
 
--- src/sudoku_board.py
+++ src/sudoku_board.py
@@ -1,11 +1,16 @@
-from tkinter import Entry, LabelFrame, StringVar
+from random import sample
+from random import seed as random_seed
+from random import shuffle
+from tkinter import Entry, Event, LabelFrame, StringVar
+from typing import Optional
 
 DIGITS = {str(i) for i in range(1, 11)}
 
 
 class SudokuBoard:
     def __init__(self):
-        self.sv = [[StringVar() for j in range(9)] for i in range(9)]
+        self.solution = [["" for _ in range(9)] for _ in range(9)]
+        self.sv = [[StringVar() for _ in range(9)] for _ in range(9)]
         for i in range(9):
             for j in range(9):
                 self.sv[i][j].trace(
@@ -16,14 +21,12 @@ class SudokuBoard:
         self.sub_boxes = [
             LabelFrame(self.frame, relief="solid") for j in range(9)
         ]
-        self.board: list[list[Entry]] = []
+        self.board: list[list[Entry]] = [[] for _ in range(9)]
         for i in range(9):
-            self.board.append([])
             for j in range(9):
-                row, col = i // 3, j // 3
                 self.board[i].append(
                     Entry(
-                        self.sub_boxes[3 * row + col],
+                        self.sub_boxes[3 * (i // 3) + (j // 3)],
                         width=2,
                         font=("Helvetica", 28),
                         justify="center",
@@ -31,7 +34,7 @@ class SudokuBoard:
                     )
                 )
                 self.board[i][j].bind(
-                    "<1>", lambda event, i=i, j=j: self.colour_box(event, i, j)
+                    "<1>", lambda event, x=i, y=j: self.colour_box(event, x, y)
                 )
                 self.board[i][j].grid(row=i, column=j)
             self.sub_boxes[i].grid(row=i // 3, column=i % 3)
@@ -39,7 +42,76 @@ class SudokuBoard:
         # Packing Frames
         self.frame.grid(row=0, column=1, padx=50, pady=10)
 
-    def detect(self, i, j):
+    def find_empty(self) -> Optional[tuple[int, int]]:
+        # Find empty position
+        for i in range(9):
+            for j in range(9):
+                if self.sv[i][j].get() == "":
+                    return i, j
+        return None
+
+    def solve_board(self, rand: bool = False) -> bool:
+        # Find Empty positions
+        empty_pos = self.find_empty()
+        if empty_pos is None:
+            return True
+        else:
+            i, j = empty_pos
+
+        # Try random digits or not
+        digits = [str(i) for i in range(1, 10)]
+        if rand:
+            shuffle(digits)
+
+        # Solve via back-tracking
+        for d in digits:
+            if self.possible(d, i, j):
+                self.sv[i][j].set(d)
+                if self.solve_board(rand=rand):
+                    return True
+                self.sv[i][j].set("")
+        return False
+
+    def generate(self, seed: Optional[int] = None, num_clues: int = 17):
+        if seed is not None:
+            random_seed(seed)
+        # Get random positions to keep
+        entry_sample = sample(range(81), num_clues)
+        keep_pos = {(x // 9, x % 9) for x in entry_sample}
+        # Refresh the board
+        for i in range(9):
+            for j in range(9):
+                self.sv[i][j].set("")
+                self.board[i][j]["state"] = "normal"
+        # Solve the board
+        is_solved = self.solve_board(rand=True)
+        # Partially erasing solved board
+        for i in range(9):
+            for j in range(9):
+                self.solution[i][j] = self.sv[i][j].get()
+                if (i, j) not in keep_pos:
+                    self.sv[i][j].set("")
+                else:
+                    self.board[i][j]["state"] = "disabled"
+                self.board[i][j]["bg"] = "white"
+        if is_solved:
+            print("Board Generated.")
+
+    def hint(self):
+        entry_sample = sample(range(81), 81)
+        pos = {(x // 9, x % 9) for x in entry_sample}
+        for i, j in pos:
+            if self.sv[i][j].get() == "":
+                self.sv[i][j].set(self.solution[i][j])
+                break
+
+    def solve(self):
+        for i in range(9):
+            for j in range(9):
+                if self.sv[i][j].get() == "":
+                    self.sv[i][j].set(self.solution[i][j])
+
+    def detect(self, i: int, j: int):
         digit = self.sv[i][j].get()
 
         # Entries must be a digit
@@ -54,7 +126,7 @@ class SudokuBoard:
         else:
             self.board[i][j]["bg"] = "#ADD8E6"
 
-    def colour_box(self, event, i, j):
+    def colour_box(self, event: Event, i: int, j: int):
         # Re-colour selected boxes
         box = 3 * (i // 3) + j // 3
         for row in range(9):
@@ -69,23 +141,23 @@ class SudokuBoard:
                     self.board[row][col]["bg"] = "#daedf4"
                 else:
                     self.board[row][col]["bg"] = "white"
-                d = self.board[row][col].get()
-                if d != "":
-                    if not self.possible(d, row, col):
+                digit = self.board[row][col].get()
+                if digit != "":
+                    if not self.possible(digit, row, col):
                         self.board[row][col]["bg"] = "#FF7F7F"
 
-    def possible(self, d, i, j):
+    def possible(self, digit: str, i: int, j: int) -> bool:
         # Check Rows
         for x in range(9):
-            if x != i and d == self.sv[x][j].get():
+            if x != i and digit == self.sv[x][j].get():
                 return False
         # Check Columns
         for y in range(9):
-            if y != j and d == self.sv[i][y].get():
+            if y != j and digit == self.sv[i][y].get():
                 return False
         # Check Box
         for x in range(3 * (i // 3), 3 * (i // 3) + 3):
             for y in range(3 * (j // 3), 3 * (j // 3) + 3):
-                if x != i and y != j and d == self.sv[x][y].get():
+                if x != i and y != j and digit == self.sv[x][y].get():
                     return False
         return True
