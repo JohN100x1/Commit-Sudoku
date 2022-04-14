d6e20cbf0ff7a65d8b80b2aaab6d1d0b8bc49321
JohN100x1 <JohN100x1@hotmail.co.uk>
2022-04-14 23:33:59 +0100

    test: added more tests for test_logic.py

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -3,6 +3,11 @@ import pytest
 from sudoku.logic import SudokuLogic
 
 
+@pytest.fixture
+def logic():
+    return SudokuLogic()
+
+
 class TestSudokuLogicIterateBoard:
     """Test SudokuLogic.iterate_board."""
 
@@ -13,10 +18,6 @@ class TestSudokuLogicIterateBoard:
 class TestSudokuLogicFindEmpty:
     """Test SudokuLogic.find_empty."""
 
-    @pytest.fixture
-    def logic(self):
-        return SudokuLogic()
-
     def test_found(self, logic):
         assert logic.find_empty() == (0, 0)
 
@@ -24,3 +25,23 @@ class TestSudokuLogicFindEmpty:
         for i, j in logic.iterate_board():
             logic.board[i][j] = 1
         assert logic.find_empty() is None
+
+
+class TestSudokuLogicResetBoard:
+    """Test SudokuLogic.reset_board."""
+
+    def test_reset(self, logic):
+        logic.board = [[1 for _ in range(9)] for _ in range(9)]
+        logic.reset_board()
+        assert logic.board == [[0 for _ in range(9)] for _ in range(9)]
+
+
+class TestSudokuLogicFillBoard:
+    """Test SudokuLogic.fill_board."""
+
+    def test_fill(self, logic):
+        solution_board = [[i for i in range(9)] for _ in range(9)]
+        logic.board = [[1 for _ in range(9)] for _ in range(9)]
+        logic.solution = solution_board
+        logic.fill_board()
+        assert logic.board == solution_board
