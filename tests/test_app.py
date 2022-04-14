876f32fc8d9c78c2417c734fab7a3442fe08bb1b
JohN100x1 <JohN100x1@hotmail.co.uk>
2022-04-14 23:01:01 +0100

    test: added test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,23 @@
-from sudoku.app import SudokuApp
+from sudoku.app import Actions, SudokuApp
 
 
-def test_sudoku_app_generate_board():
-    app = SudokuApp()
+class TestSudokuAppButtonAction:
+    """Test SudokuApp.button_action."""
+
+    def test_generate_board(self, mocker):
+        mock = mocker.patch("sudoku.app.SudokuBoard.generate")
+        app = SudokuApp()
+        app.button_action(Actions.NEW_GAME)
+        assert mock.call_count == 2
+
+    def test_solve_board(self, mocker):
+        mock = mocker.patch("sudoku.app.SudokuBoard.solve")
+        app = SudokuApp()
+        app.button_action(Actions.SOLVE)
+        assert mock.call_count == 1
+
+    def test_hint_board(self, mocker):
+        mock = mocker.patch("sudoku.app.SudokuBoard.hint")
+        app = SudokuApp()
+        app.button_action(Actions.HINT)
+        assert mock.call_count == 1
