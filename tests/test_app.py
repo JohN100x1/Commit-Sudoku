d99929d75839d5e63c2a0247296a0e132d11f457
JohN100x1 <JohN100x1@hotmail.co.uk>
2022-04-14 23:27:55 +0100

    test: update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,23 +1,24 @@
 from sudoku.app import Actions, SudokuApp
+from sudoku.view import SudokuBoard
 
 
 class TestSudokuAppButtonAction:
     """Test SudokuApp.button_action."""
 
     def test_generate_board(self, mocker):
-        mock = mocker.patch("sudoku.app.SudokuBoard.generate")
+        mock = mocker.spy(SudokuBoard, "generate")
         app = SudokuApp()
         app.button_action(Actions.NEW_GAME)
         assert mock.call_count == 2
 
     def test_solve_board(self, mocker):
-        mock = mocker.patch("sudoku.app.SudokuBoard.solve")
+        mock = mocker.spy(SudokuBoard, "solve")
         app = SudokuApp()
         app.button_action(Actions.SOLVE)
         assert mock.call_count == 1
 
     def test_hint_board(self, mocker):
-        mock = mocker.patch("sudoku.app.SudokuBoard.hint")
+        mock = mocker.spy(SudokuBoard, "hint")
         app = SudokuApp()
         app.button_action(Actions.HINT)
         assert mock.call_count == 1
